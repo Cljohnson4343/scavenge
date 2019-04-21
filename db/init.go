c053127bf43e220b29707a4a512c55df0acc66b9
Chris Johnson <cljohnson4343@gmail.com>
2019-04-21 07:47:46 -0500

    moved the db pkg's init() function to its own file.

---
+++ db/init.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+	govalidator.TagMap["positive"] = govalidator.Validator(func(str string) bool {
+		v, err := strconv.Atoi(str)
+		if err != nil {
+			return false
+		}
+
+		return v > 0
+	})
+	govalidator.TagMap["isNil"] = govalidator.Validator(func(str string) bool {
+		return false
+	})
+	govalidator.CustomTypeTagMap.Set("startTimeBeforeEndTime", govalidator.CustomTypeValidator(func(i interface{}, context interface{}) bool {
+		switch v := context.(type) {
+		case HuntDB:
+			return v.StartTime.Before(v.EndTime)
+		}
+
+		return false
+	}))
+	govalidator.CustomTypeTagMap.Set("timeNotPast", govalidator.CustomTypeValidator(func(i interface{}, context interface{}) bool {
+		switch v := i.(type) {
+		case time.Time:
+			return v.After(time.Now())
+		}
+		return false
+	}))
+}
--- db/team.go
+++ db/team.go
@@ -17,36 +17,6 @@ import (
 // TeamTbl is the name of the db table for the TeamDB struct
 const TeamTbl string = "teams"
 
-func init() {
-	govalidator.SetFieldsRequiredByDefault(true)
-	govalidator.TagMap["positive"] = govalidator.Validator(func(str string) bool {
-		v, err := strconv.Atoi(str)
-		if err != nil {
-			return false
-		}
-
-		return v > 0
-	})
-	govalidator.TagMap["isNil"] = govalidator.Validator(func(str string) bool {
-		return false
-	})
-	govalidator.CustomTypeTagMap.Set("startTimeBeforeEndTime", govalidator.CustomTypeValidator(func(i interface{}, context interface{}) bool {
-		switch v := context.(type) {
-		case HuntDB:
-			return v.StartTime.Before(v.EndTime)
-		}
-
-		return false
-	}))
-	govalidator.CustomTypeTagMap.Set("timeNotPast", govalidator.CustomTypeValidator(func(i interface{}, context interface{}) bool {
-		switch v := i.(type) {
-		case time.Time:
-			return v.After(time.Now())
-		}
-		return false
-	}))
-}
-
 // A TeamDB is a representation of a row in the teams table
 //
 // swagger:model TeamDB
