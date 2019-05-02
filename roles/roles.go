0a9564e75c0bdbe8acd4729ab7f09579fb8a859c
Chris Johnson <cljohnson4343@gmail.com>
2019-05-02 13:34:55 -0500

    moved all role definittions out of entities packages and into roles package.

--- roles/roles.go
+++ roles/roles.go
@@ -1,6 +1,7 @@
 package roles
 
 import (
+	"fmt"
 	"net/http"
 	"regexp"
 	"strings"
@@ -13,11 +14,23 @@ type Role struct {
 	Child       *Role
 }
 
+// New returns a new role
+func New(role string, entityID int) *Role {
+	return roleToGenerator[role](entityID)
+}
+
 // Add adds the childRole to the given role. This is how inheritence relationships
 // are modeled
 func (r *Role) Add(childRole *Role) {
 	r.Child = childRole
 }
+
+/*
+// AddTo adds the role to the given user
+func (r *Role) AddTo(userID int) {
+
+}
+*/
 
 // Authorized returns whether or not the role contains a permission that is
 // authorized for the given req
@@ -50,4 +63,174 @@ func (p *Permission) Authorized(r *http.Request) bool {
 
 	regEx := regexp.MustCompile(p.URLRegex)
 	return regEx.MatchString(r.URL.Path)
+}
+
+// GeneratePermission generates permission for the given route and entity id
+func GeneratePermission(perm string, entityID int) *Permission {
+	var regex string
+	if strings.Contains(permToFormattedRegex[perm], "%") {
+		regex = fmt.Sprintf(permToFormattedRegex[perm], entityID)
+	} else {
+		regex = permToFormattedRegex[perm]
+	}
+	permission := Permission{
+		Method:   strings.Split(perm, "_")[0],
+		URLRegex: regex,
+	}
+
+	return &permission
+}
+
+var permToFormattedRegex = map[string]string{
+	// team endpoints
+	"get_teams":           `^teams/$`,
+	"get_team":            `^teams/%d$`,
+	"get_points":          `^teams/%d/points/$`,
+	"get_players":         `^teams/%d/players/$`,
+	"post_player":         `^teams/%d/players/$`,
+	"delete_player":       `^teams/%d/players/\d+$`,
+	"delete_team":         `^teams/%d$`,
+	"post_team":           `^teams/$`,
+	"patch_team":          `^teams/%d$`,
+	"get_locations":       `^teams/%d/locations/$`,
+	"post_location":       `^teams/%d/locations/$`,
+	"delete_location":     `^teams/%d/locations/\d+$`,
+	"get_media":           `^teams/%d/media/$`,
+	"post_media":          `^teams/%d/media/$`,
+	"delete_media":        `^teams/%d/media/\d+$`,
+	"post_teams_populate": `^teams/populate/$`,
+
+	// user endpoints
+	"get_user":    `^users/%d$`,
+	"post_login":  `^users/login/$`,
+	"post_logout": `^users/logout/$`,
+	"post_user":   `^users/$`,
+	"delete_user": `^users/%d$`,
+	"patch_user":  `^users/%d$`,
+
+	// hunt endpoints
+	"get_hunts":           `^hunts/$`,
+	"get_hunt":            `^hunts/%d$`,
+	"post_hunt":           `^hunts/$`,
+	"delete_hunt":         `^hunts/%d$`,
+	"patch_hunt":          `^hunts/%d$`,
+	"post_hunts_populate": `^hunts/populate/$`,
+	"get_items":           `^hunts/%d/items/$`,
+	"delete_item":         `^hunts/%d/items/\d+$`,
+	"post_item":           `^hunts/%d/items/$`,
+	"patch_item":          `^hunts/%d/items/\d+$`,
+}
+
+// PermToRole maps a permissions key to a role
+var PermToRole = map[string]string{
+	// team endpoints
+	"get_teams":           `admin`,
+	"get_team":            `user`,
+	"get_points":          `hunt_member`,
+	"get_players":         `user`,
+	"post_player":         ``,
+	"delete_player":       `team_editor`,
+	"delete_team":         `team_owner`,
+	"post_team":           ``,
+	"patch_team":          `team_editor`,
+	"get_locations":       `hunt_member`,
+	"post_location":       `team_member`,
+	"delete_location":     `admin`,
+	"get_media":           `hunt_member`,
+	"post_media":          `team_member`,
+	"delete_media":        `team_member`,
+	"post_teams_populate": `admin`,
+
+	// user endpoints
+	"get_user":    `public`,
+	"post_login":  `user`,
+	"post_logout": `user`,
+	"post_user":   `public`,
+	"delete_user": `user_owner`,
+	"patch_user":  `user_owner`,
+
+	// hunt endpoints
+	"get_hunts":           `user`,
+	"get_hunt":            `user`,
+	"post_hunt":           `user`,
+	"delete_hunt":         `hunt_owner`,
+	"patch_hunt":          `hunt_editor`,
+	"post_hunts_populate": `admin`,
+	"get_items":           `user`,
+	"delete_item":         `hunt_editor`,
+	"post_item":           `hunt_editor`,
+	"patch_item":          `hunt_editor`,
+}
+
+// the team role relationships look like: Owner -> Editor -> Member -> HuntMember -> User
+var roleToGenerator = map[string]func(int) *Role{
+	//	"admin":       genAdminRole,
+	"hunt_owner":  genHuntOwnerRole,
+	"hunt_editor": genHuntEditorRole,
+	"hunt_member": genHuntMemberRole,
+	"team_owner":  genTeamOwnerRole,
+	"team_editor": genTeamEditorRole,
+	"team_member": genTeamMemberRole,
+	"user":        genUserRole,
+	"user_owner":  genUserOwnerRole,
+}
+
+func genRole(name string, id int) *Role {
+	role := Role{
+		Name:        fmt.Sprintf("%s_%d", name, id),
+		Permissions: make([]*Permission, 0),
+	}
+	// create role specific permissions
+	for k, v := range PermToRole {
+		if v == name {
+			role.Permissions = append(role.Permissions, GeneratePermission(k, id))
+		}
+	}
+
+	return &role
+}
+
+func genHuntOwnerRole(id int) *Role {
+	owner := genRole("hunt_owner", id)
+	owner.Add(genHuntEditorRole(id))
+
+	return owner
+}
+
+func genHuntEditorRole(id int) *Role {
+	editor := genRole("hunt_editor", id)
+	editor.Add(genHuntMemberRole(id))
+
+	return editor
+}
+
+func genHuntMemberRole(id int) *Role {
+	editor := genRole("hunt_member", id)
+
+	return editor
+}
+func genTeamOwnerRole(id int) *Role {
+	owner := genRole("team_owner", id)
+	owner.Add(genTeamEditorRole(id))
+
+	return owner
+}
+
+func genTeamEditorRole(id int) *Role {
+	editor := genRole("team_editor", id)
+	editor.Add(genTeamMemberRole(id))
+
+	return editor
+}
+
+func genTeamMemberRole(id int) *Role {
+	return genRole("team_member", id)
+}
+
+func genUserRole(id int) *Role {
+	return genRole("user", id)
+}
+
+func genUserOwnerRole(id int) *Role {
+	return genRole("user_owner", id)
 }
---
+++ roles/roles_test.go
@@ -0,0 +1,473 @@
+// +build unit
+
+package roles_test
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/cljohnson4343/scavenge/roles"
+)
+
+var permToRoutes = map[string]string{
+	// teams routes
+	"get_teams":           `teams/`,
+	"get_team":            `teams/%d`,
+	"get_points":          `teams/%d/points/`,
+	"get_players":         `teams/%d/players/`,
+	"post_player":         `teams/%d/players/`,
+	"delete_player":       `teams/%d/players/43`,
+	"delete_team":         `teams/%d`,
+	"post_team":           `teams/`,
+	"patch_team":          `teams/%d`,
+	"get_locations":       `teams/%d/locations/`,
+	"post_location":       `teams/%d/locations/`,
+	"delete_location":     `teams/%d/locations/43`,
+	"get_media":           `teams/%d/media/`,
+	"post_media":          `teams/%d/media/`,
+	"delete_media":        `teams/%d/media/43`,
+	"post_teams_populate": `teams/populate/`,
+
+	// hunts routes
+	"get_hunts":           `hunts/`,
+	"get_hunt":            `hunts/%d`,
+	"post_hunt":           `hunts/`,
+	"delete_hunt":         `hunts/%d`,
+	"patch_hunt":          `hunts/%d`,
+	"post_hunts_populate": `hunts/populate/`,
+	"get_items":           `hunts/%d/items/`,
+	"delete_item":         `hunts/%d/items/43`,
+	"post_item":           `hunts/%d/items/`,
+	"patch_item":          `hunts/%d/items/43`,
+
+	// users routes
+	"get_user":    `users/%d`,
+	"post_login":  `users/login/`,
+	"post_logout": `users/logout/`,
+	"post_user":   `users/`,
+	"delete_user": `users/%d`,
+	"patch_user":  `users/%d`,
+}
+
+type testCase struct {
+	name     string
+	expected bool
+	req      *http.Request
+}
+
+func getReq(id int, key string) *http.Request {
+	var url string
+	if strings.Contains(permToRoutes[key], "%") {
+		url = fmt.Sprintf(permToRoutes[key], id)
+	} else {
+		url = permToRoutes[key]
+	}
+
+	req, err := http.NewRequest(strings.Split(key, "_")[0], url, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return req
+}
+
+func generatePermissionCases(perm string, id int) []*testCase {
+	cases := make([]*testCase, 0, len(permToRoutes))
+
+	for k, _ := range permToRoutes {
+		c := testCase{
+			name:     k,
+			expected: false,
+			req:      getReq(id, k),
+		}
+		if k == perm {
+			c.expected = true
+		}
+
+		cases = append(cases, &c)
+	}
+
+	return cases
+}
+
+func testGeneratePermission(t *testing.T, perm string, optionalCase *testCase) {
+	permission := roles.GeneratePermission(perm, 1)
+	cases := generatePermissionCases(perm, 1)
+	if optionalCase != nil {
+		cases = append(cases, optionalCase)
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := permission.Authorized(c.req)
+			if got != c.expected {
+				t.Errorf(
+					"epected %v got %v\n\tregex: %s\n\turl: %s\n",
+					c.expected,
+					got,
+					permission.URLRegex,
+					c.req.URL.Path,
+				)
+			}
+		})
+	}
+}
+
+func TestGenerateGetTeams(t *testing.T) {
+	testGeneratePermission(t, "get_teams", nil)
+}
+
+func TestGenerateGetTeam(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_get_team",
+		req:      getReq(43, "get_team"),
+		expected: false,
+	}
+	testGeneratePermission(t, "get_team", optional)
+}
+
+func TestGenerateGetPoints(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_get_points",
+		req:      getReq(43, "get_points"),
+		expected: false,
+	}
+
+	testGeneratePermission(t, "get_points", optional)
+}
+
+func TestGenerateGetPlayers(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_get_players",
+		req:      getReq(43, "get_players"),
+		expected: false,
+	}
+	testGeneratePermission(t, "get_players", optional)
+}
+
+func TestGeneratePostPlayer(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_post_player",
+		req:      getReq(43, "post_player"),
+		expected: false,
+	}
+	testGeneratePermission(t, "post_player", optional)
+}
+
+func TestGenerateDeletePlayer(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_delete_player",
+		req:      getReq(43, "delete_player"),
+		expected: false,
+	}
+
+	testGeneratePermission(t, "delete_player", optional)
+}
+
+func TestGenerateDeleteTeam(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_delete_team",
+		req:      getReq(43, "delete_team"),
+		expected: false,
+	}
+	testGeneratePermission(t, "delete_team", optional)
+}
+
+func TestGeneratePostTeam(t *testing.T) {
+	testGeneratePermission(t, "post_team", nil)
+}
+
+func TestGeneratePatchTeam(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_patch_team",
+		req:      getReq(43, "patch_team"),
+		expected: false,
+	}
+	testGeneratePermission(t, "patch_team", optional)
+}
+
+func TestGenerateGetLocations(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_get_locations",
+		req:      getReq(43, "get_locations"),
+		expected: false,
+	}
+	testGeneratePermission(t, "get_locations", optional)
+}
+
+func TestGeneratePostLocation(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_post_location",
+		req:      getReq(43, "post_location"),
+		expected: false,
+	}
+	testGeneratePermission(t, "post_location", optional)
+}
+
+func TestGenerateDeleteLocation(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_delete_location",
+		req:      getReq(43, "delete_location"),
+		expected: false,
+	}
+	testGeneratePermission(t, "delete_location", optional)
+}
+
+func TestGenerateGetMedia(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_get_media",
+		req:      getReq(43, "get_media"),
+		expected: false,
+	}
+	testGeneratePermission(t, "get_media", optional)
+}
+
+func TestGeneratePostMedia(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_post_media",
+		req:      getReq(43, "post_media"),
+		expected: false,
+	}
+	testGeneratePermission(t, "post_media", optional)
+}
+
+func TestGenerateDeleteMedia(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_delete_media",
+		req:      getReq(43, "delete_media"),
+		expected: false,
+	}
+	testGeneratePermission(t, "delete_media", optional)
+}
+
+func TestGeneratePostTeamsPopulate(t *testing.T) {
+	testGeneratePermission(t, "post_teams_populate", nil)
+}
+
+func TestGenerateGetUser(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_get_user",
+		req:      getReq(43, "get_user"),
+		expected: false,
+	}
+	testGeneratePermission(t, "get_user", optional)
+}
+
+func TestGeneratePostLogin(t *testing.T) {
+	testGeneratePermission(t, "post_login", nil)
+}
+
+func TestGeneratePostLogout(t *testing.T) {
+	testGeneratePermission(t, "post_logout", nil)
+}
+
+func TestGeneratePostUser(t *testing.T) {
+	testGeneratePermission(t, "post_user", nil)
+}
+
+func TestGenerateDeleteUser(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_delete_user",
+		req:      getReq(43, "delete_user"),
+		expected: false,
+	}
+	testGeneratePermission(t, "delete_user", optional)
+}
+
+func TestGeneratePatchUser(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_patch_user",
+		req:      getReq(43, "patch_user"),
+		expected: false,
+	}
+	testGeneratePermission(t, "patch_user", optional)
+}
+
+func TestGenerateGetHunts(t *testing.T) {
+	testGeneratePermission(t, "get_hunts", nil)
+}
+
+func TestGenerateGetHunt(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_get_hunt",
+		req:      getReq(43, "get_hunt"),
+		expected: false,
+	}
+	testGeneratePermission(t, "get_hunt", optional)
+}
+
+func TestGeneratePostHunt(t *testing.T) {
+	testGeneratePermission(t, "post_hunt", nil)
+}
+
+func TestGenerateDeleteHunt(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_delete_hunt",
+		req:      getReq(43, "delete_hunt"),
+		expected: false,
+	}
+	testGeneratePermission(t, "delete_hunt", optional)
+}
+
+func TestGeneratePatchHunt(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_patch_hunt",
+		req:      getReq(43, "patch_hunt"),
+		expected: false,
+	}
+	testGeneratePermission(t, "patch_hunt", optional)
+}
+
+func TestGeneratePostHuntsPopulate(t *testing.T) {
+	testGeneratePermission(t, "post_hunts_populate", nil)
+}
+
+func TestGenerateGetItems(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_get_items",
+		req:      getReq(43, "get_items"),
+		expected: false,
+	}
+	testGeneratePermission(t, "get_items", optional)
+}
+
+func TestGenerateDeleteItem(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_delete_item",
+		req:      getReq(43, "delete_item"),
+		expected: false,
+	}
+	testGeneratePermission(t, "delete_item", optional)
+}
+
+func TestGeneratePostItem(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_post_item",
+		req:      getReq(43, "post_item"),
+		expected: false,
+	}
+	testGeneratePermission(t, "post_item", optional)
+}
+
+func TestGeneratePatchItem(t *testing.T) {
+	optional := &testCase{
+		name:     "wrong_id_patch_item",
+		req:      getReq(43, "patch_item"),
+		expected: false,
+	}
+	testGeneratePermission(t, "patch_item", optional)
+}
+
+//
+// role testing
+//
+
+func generateRoleCases(role string, id int) []*testCase {
+	cases := make([]*testCase, 0, len(permToRoutes))
+
+	for k, _ := range permToRoutes {
+		c := testCase{
+			name:     k,
+			expected: getExpected(role, k),
+			req:      getReq(id, k),
+		}
+
+		cases = append(cases, &c)
+	}
+
+	return cases
+}
+
+func getExpected(role, permKey string) bool {
+	switch role {
+	case "team_owner":
+		if roles.PermToRole[permKey] == role ||
+			roles.PermToRole[permKey] == "team_editor" ||
+			roles.PermToRole[permKey] == "team_member" {
+			return true
+		}
+	case "team_editor":
+		if roles.PermToRole[permKey] == role ||
+			roles.PermToRole[permKey] == "team_member" {
+			return true
+		}
+	case "team_member":
+		if roles.PermToRole[permKey] == role {
+			return true
+		}
+	case "hunt_owner":
+		if roles.PermToRole[permKey] == role ||
+			roles.PermToRole[permKey] == "hunt_editor" ||
+			roles.PermToRole[permKey] == "hunt_member" {
+			return true
+		}
+	case "hunt_editor":
+		if roles.PermToRole[permKey] == role ||
+			roles.PermToRole[permKey] == "hunt_member" {
+			return true
+		}
+	case "hunt_member":
+		if roles.PermToRole[permKey] == role {
+			return true
+		}
+	case "user":
+		if roles.PermToRole[permKey] == role {
+			return true
+		}
+	case "user_owner":
+		if roles.PermToRole[permKey] == role {
+			return true
+		}
+	}
+
+	return false
+}
+
+func testGenerateRole(t *testing.T, name string) {
+	role := roles.New(name, 1)
+	cases := generateRoleCases(name, 1)
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := role.Authorized(c.req)
+
+			if got != c.expected {
+				t.Errorf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGenerateTeamOwnerRole(t *testing.T) {
+	testGenerateRole(t, "team_owner")
+}
+
+func TestGenerateTeamEditorRole(t *testing.T) {
+	testGenerateRole(t, "team_editor")
+}
+
+func TestGenerateTeamMemberRole(t *testing.T) {
+	testGenerateRole(t, "team_member")
+}
+
+func TestGenerateHuntOwnerRole(t *testing.T) {
+	testGenerateRole(t, "hunt_owner")
+}
+
+func TestGenerateHuntEditorRole(t *testing.T) {
+	testGenerateRole(t, "hunt_editor")
+}
+
+func TestGenerateHuntMemberRole(t *testing.T) {
+	testGenerateRole(t, "hunt_member")
+}
+
+func TestGenerateUserRole(t *testing.T) {
+	testGenerateRole(t, "user")
+}
+
+func TestGenerateUserOwnerRole(t *testing.T) {
+	testGenerateRole(t, "user_owner")
+}
