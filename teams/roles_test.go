b1954259ab9525912e2dd2dbcea04b05f24e21b9
Chris Johnson <cljohnson4343@gmail.com>
2019-05-02 00:08:09 -0500

    added tests for generate permissions.

--- teams/roles_test.go
+++ teams/roles_test.go
@@ -18,9 +18,9 @@ var routes = map[string]string{
 	"get_players":     `/%d/players/`,
 	"post_player":     `/%d/players/`,
 	"delete_player":   `/%d/players/43`,
-	"delete_team":     `/43`,
+	"delete_team":     `/%d`,
 	"post_team":       `/`,
-	"patch_team":      `/43`,
+	"patch_team":      `/%d`,
 	"get_locations":   `/%d/locations/`,
 	"post_location":   `/%d/locations/`,
 	"delete_location": `/%d/locations/43`,
@@ -136,6 +136,224 @@ func TestGenerateGetPlayers(t *testing.T) {
 		req:      getReq(43, "get_players"),
 		expected: false,
 	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGeneratePostPlayer(t *testing.T) {
+	perm := teams.GeneratePermission("post_player", 1)
+	cases := append(generateCases("post_player", 1), &testCase{
+		name:     "wrong_id_post_player",
+		req:      getReq(43, "post_player"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGenerateDeletePlayer(t *testing.T) {
+	perm := teams.GeneratePermission("delete_player", 1)
+	cases := append(generateCases("delete_player", 1), &testCase{
+		name:     "wrong_id_delete_player",
+		req:      getReq(43, "delete_player"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGenerateDeleteTeam(t *testing.T) {
+	perm := teams.GeneratePermission("delete_team", 1)
+	cases := append(generateCases("delete_team", 1), &testCase{
+		name:     "wrong_id_delete_team",
+		req:      getReq(43, "delete_team"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+func TestGeneratePostTeam(t *testing.T) {
+	perm := teams.GeneratePermission("post_team", 1)
+	cases := generateCases("post_team", 1)
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGeneratePatchTeam(t *testing.T) {
+	perm := teams.GeneratePermission("patch_team", 1)
+	cases := append(generateCases("patch_team", 1), &testCase{
+		name:     "wrong_id_patch_team",
+		req:      getReq(43, "patch_team"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGenerateGetLocations(t *testing.T) {
+	perm := teams.GeneratePermission("get_locations", 1)
+	cases := append(generateCases("get_locations", 1), &testCase{
+		name:     "wrong_id_get_locations",
+		req:      getReq(43, "get_locations"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGeneratePostLocation(t *testing.T) {
+	perm := teams.GeneratePermission("post_location", 1)
+	cases := append(generateCases("post_location", 1), &testCase{
+		name:     "wrong_id_post_location",
+		req:      getReq(43, "post_location"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGenerateDeleteLocation(t *testing.T) {
+	perm := teams.GeneratePermission("delete_location", 1)
+	cases := append(generateCases("delete_location", 1), &testCase{
+		name:     "wrong_id_delete_location",
+		req:      getReq(43, "delete_location"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGenerateGetMedia(t *testing.T) {
+	perm := teams.GeneratePermission("get_media", 1)
+	cases := append(generateCases("get_media", 1), &testCase{
+		name:     "wrong_id_get_media",
+		req:      getReq(43, "get_media"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+func TestGeneratePostMedia(t *testing.T) {
+	perm := teams.GeneratePermission("post_media", 1)
+	cases := append(generateCases("post_media", 1), &testCase{
+		name:     "wrong_id_post_media",
+		req:      getReq(43, "post_media"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGenerateDeleteMedia(t *testing.T) {
+	perm := teams.GeneratePermission("delete_media", 1)
+	cases := append(generateCases("delete_media", 1), &testCase{
+		name:     "wrong_id_delete_media",
+		req:      getReq(43, "delete_media"),
+		expected: false,
+	})
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := perm.Authorized(c.req)
+
+			if got != c.expected {
+				t.Fatalf("expected %v got %v", c.expected, got)
+			}
+		})
+	}
+}
+
+func TestGeneratePostPopulate(t *testing.T) {
+	perm := teams.GeneratePermission("post_populate", 1)
+	cases := generateCases("post_populate", 1)
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
