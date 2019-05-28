a50beedd511be60bfb58c4041d9bdcc99f7a9984
chris johnson <cljohnson4343@gmail.com>
2019-05-27 23:38:07 -0500

    added middleware for logging.

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cljohnson4343/scavenge/teams"
 	"github.com/cljohnson4343/scavenge/users"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 // Routes inits a router
@@ -14,6 +15,8 @@ func Routes(env *config.Env) *chi.Mux {
 	router := chi.NewRouter()
 
 	router.Route(config.BaseAPIURL, func(r chi.Router) {
+		r.Use(middleware.RequestID)
+		r.Use(middleware.Logger)
 		r.Use(response.AllowCORS)
 
 		r.Mount("/hunts", hunts.Routes(env))
